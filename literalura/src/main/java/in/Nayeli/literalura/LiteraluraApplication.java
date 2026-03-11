9277b7e4774d00b5d839bfc344218d4fa2d7d46f
Nayeli <darelnayeli@gmail.com>
2026-03-10 19:06:06 -0600

    Add Principal class and console menu

--- literalura/src/main/java/in/Nayeli/literalura/LiteraluraApplication.java
+++ literalura/src/main/java/in/Nayeli/literalura/LiteraluraApplication.java
@@ -3,16 +3,18 @@ package in.Nayeli.literalura;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import in.Nayeli.literalura.principal.Principal;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class LiteraluraApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(LiteraluraApplication.class, args);
+
 		System.out.println("Proyecto Literalura iniciado correctamente");
 		System.out.println("Preparado para consumir la API de libros");
 
+		Principal principal = new Principal();
+		principal.muestraMenu();
 	}
-
 }
---
+++ literalura/src/main/java/in/Nayeli/literalura/principal/Principal.java
@@ -0,0 +1,14 @@
+package in.Nayeli.literalura.principal;
+
+public class Principal {
+
+    public void muestraMenu() {
+        System.out.println("=== LITERALURA ===");
+        System.out.println("1 - Buscar libro por título");
+        System.out.println("2 - Listar libros registrados");
+        System.out.println("3 - Listar autores registrados");
+        System.out.println("4 - Listar autores vivos en un año");
+        System.out.println("5 - Listar libros por idioma");
+        System.out.println("0 - Salir");
+    }
+}
